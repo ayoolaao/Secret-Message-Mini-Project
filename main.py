8573d5d5948ee9df071a820efdea7bc280e4d224
Ayoolaao <ao.abimbola@gmail.com>
2016-11-24 20:42:35 -0600

    Secret Message - Mini Project. second draft

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ import os
 def rename_files ():
 
     file_list = os.listdir("/Users/ayoolaao/Google Drive/Docs_Projects_Portfolios/Portfolios/Full Stack Web Developer Nanodegree/Intro to python/Secret Message Mini-Project/msg")
-    print(file_list)
-
-    print(os.getcwd())
     os.chdir("/Users/ayoolaao/Google Drive/Docs_Projects_Portfolios/Portfolios/Full Stack Web Developer Nanodegree/Intro to python/Secret Message Mini-Project/msg")
-    print(os.getcwd())
 
-    for i in file_list:
+    for files in file_list:
         a = "0123456789"
-        b = i.translate(None, a)
+        b = files.translate(None, a)
 
-        os.rename(i, b)
+        os.rename(files, b)
         print("Done")
 
 
